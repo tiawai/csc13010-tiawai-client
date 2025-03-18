e14d41d215ac49370aef94ef39bcdf7e80ff72d1
Npeka <npkhang22@clc.fitus.edu.vn>
2025-03-18 22:36:06 +0700

    refactor: update admin layout

--- client/src/app/(user)/layout.tsx
+++ client/src/app/(user)/layout.tsx
@@ -1,4 +1,3 @@
-import { usePathname } from 'next/navigation';
 import { Suspense } from 'react';
 import Header from '@/components/common/header';
 import Footer from '@/components/common/footer';
@@ -9,22 +8,19 @@ export default async function UserLayout({
 }: {
     children: React.ReactNode;
 }) {
-    const pathname = usePathname();
-    const hideFooter =
-        pathname.startsWith('/flashcard/') && pathname !== '/flashcard';
     return (
-        <main>
+        <>
             <Header />
 
             <Suspense>
                 <ChatButton />
             </Suspense>
 
-            <div className="m-auto !mt-28 min-h-[calc(100vh-300px)] max-w-[1320px] px-4">
+            <main className="m-auto !mt-28 min-h-[calc(100vh-300px)] max-w-[1320px] px-4">
                 <Suspense>{children}</Suspense>
-            </div>
+            </main>
 
             <Footer />
-        </main>
+        </>
     );
 }
