b0dc65d1757aa886ae67aa466b856468cd4e81a1
Npeka <128128732+Npeka@users.noreply.github.com>
2025-04-18 22:19:15 +0700

    feat: integrated create test for teacher and add template.xlsx for payout

--- client/src/types/test.type.ts
+++ client/src/types/test.type.ts
@@ -28,6 +28,7 @@ export interface TestResponseDto {
 }
 
 export interface CreateTestDto {
+    classroomId?: string;
     title: string;
     type: TestType;
     startDate: string;
@@ -37,11 +38,13 @@ export interface CreateTestDto {
 }
 
 export interface CreateNationalTestDto {
+    classroomId?: string;
     test: CreateTestDto;
     questions: CreateQuestionDto[];
 }
 
 export interface CreateToeicListeningTestDto {
+    classroomId?: string;
     audioUrl: string;
     test: CreateTestDto;
 }
