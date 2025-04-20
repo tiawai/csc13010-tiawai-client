8471d55ccef5aa481702e1a0739bb235ffca12ab
JonasAugust12 <cas.nmtoan@gmail.com>
2025-04-20 23:27:51 +0700

    fix: session, profile, hamepage

--- client/src/app/(user)/layout.tsx
+++ client/src/app/(user)/layout.tsx
@@ -1,7 +1,6 @@
 import Header from '@/components/common/header';
 import Footer from '@/components/common/footer';
 import ChatButton from '@/ui/chat-button';
-import { SessionWrapper } from '@/components/common/wrapper';
 
 export default async function UserLayout({
     children,
@@ -10,14 +9,12 @@ export default async function UserLayout({
 }) {
     return (
         <div className="flex h-lvh min-h-lvh flex-col overflow-x-clip">
-            <SessionWrapper>
-                <Header />
-                <ChatButton />
-                <main className="mx-auto w-full max-w-[1320px] grow px-4 pb-4 pt-28">
-                    {children}
-                </main>
-                <Footer />
-            </SessionWrapper>
+            <Header />
+            <ChatButton />
+            <main className="mx-auto w-full max-w-[1320px] grow px-4 pb-4 pt-28">
+                {children}
+            </main>
+            <Footer />
         </div>
     );
 }
--- client/src/components/common/loading.tsx
+++ client/src/components/common/loading.tsx
@@ -1,9 +1,15 @@
 import { Spin } from 'antd';
 import Paragraph from 'antd/es/typography/Paragraph';
+import clsx from 'clsx';
 
-export const Loading = () => {
+export const Loading = ({ className }: { className?: string }) => {
     return (
-        <div className="flex h-full flex-col items-center justify-center gap-2">
+        <div
+            className={clsx(
+                'flex h-full flex-col items-center justify-center gap-2',
+                className,
+            )}
+        >
             <Spin size="large" tip="Loading..." />
             <Paragraph>Loading...</Paragraph>
         </div>
