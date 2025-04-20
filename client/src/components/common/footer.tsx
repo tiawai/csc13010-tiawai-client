c96d11f1da66bcd2aa3af67c140198b7e7e38c3c
Nguyen Duy Hoang <ndhoang22@clc.fitus.edu.vn>
2025-04-20 21:06:19 +0700

    feat: update footer component to display services based on user role

--- client/src/components/common/footer.tsx
+++ client/src/components/common/footer.tsx
@@ -1,3 +1,4 @@
+'use client';
 import Link from 'next/link';
 import { Row, Col } from 'antd';
 import {
@@ -8,12 +9,27 @@ import {
 } from '@ant-design/icons';
 import Image from 'next/image';
 import logo from '@public/logo.svg';
+import { useAppSelector } from '@/lib/hooks/hook';
+import { selectUser } from '@/lib/slices/auth.slice';
+import { Role } from '@/types/user.type';
+import { TestType } from '@/types/test.type';
 
-const services = [
-    { name: 'Kỹ Năng IELTS', link: '/' },
-    { name: 'Ôn Thi THPTQG', link: '/' },
-    { name: 'Flashcard', link: '/' },
-    { name: 'Lớp Học', link: '/' },
+const studentServices = [
+    {
+        name: 'Kỹ Năng TOEIC',
+        link: `/student/test?type=${encodeURIComponent(TestType.TOEIC_LISTENING)}`,
+    },
+    {
+        name: 'Ôn Thi THPTQG',
+        link: `/student/test?type=${encodeURIComponent(TestType.NATIONAL_TEST)}`,
+    },
+    { name: 'Flashcard', link: '/student/flashcard' },
+    { name: 'Lớp Học', link: '/student/class' },
+];
+
+const teacherServices = [
+    { name: 'Quản lý lớp học', link: '/teacher/classroom' },
+    { name: 'Quản lý Bài tập/ Đề Thi', link: '/teacher/exam' },
 ];
 
 const contactInfo = [
@@ -36,6 +52,10 @@ const contactInfo = [
 ];
 
 const Footer = () => {
+    const user = useAppSelector(selectUser);
+    const services =
+        user?.role === Role.TEACHER ? teacherServices : studentServices;
+
     return (
         <footer className="mt-5 cursor-default bg-[#E9DAE9] py-5">
             <div className="mx-auto max-w-[1320px] px-6">
@@ -67,7 +87,11 @@ const Footer = () => {
                             {services.map((service, index) => (
                                 <li key={index}>
                                     <Link
-                                        href={service.link}
+                                        href={
+                                            user?.role === Role.GUEST
+                                                ? `/auth/sign-in`
+                                                : service.link
+                                        }
                                         className="text-black transition hover:text-[#5369A1]"
                                     >
                                         › &nbsp;&nbsp;&nbsp;{service.name}
